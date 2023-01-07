14735551cdcb52138221b293537227a78cbf5b38
iqpizza6349 <iqpizza6349@gmail.com>
2023-01-08 08:03:31 +0900

    :recycle: :: (#5) refactor type of generics

--- Spi-Common/src/main/java/com/dgsw/cns/global/domain/BaseTimeEntity.java
+++ Spi-Common/src/main/java/com/dgsw/cns/global/domain/BaseTimeEntity.java
@@ -14,15 +14,15 @@ import java.time.LocalDateTime;
  * 데이터베이스 삽입(추가)할 때 CURRENT_TIMESTAMP 을 default 로 주입되도록 하는 추상 클래스
  * {@link BaseIdEntity} 을 상속하여, {@link Serializable}를 구현한 클래스를 제네릭으로
  * 구현해야한다.
- * @param <Entity> {@link Serializable}을 구현한 클래스만 허용하며, {@link BaseIdEntity}에
+ * @param <T> {@link Serializable}을 구현한 클래스만 허용하며, {@link BaseIdEntity}에
  *                적용되는 param입니다.
  */
 @Getter
 @SuperBuilder
 @MappedSuperclass
 @NoArgsConstructor
-public abstract class BaseTimeEntity<Entity extends Serializable>
-        extends BaseIdEntity<Entity> {
+public abstract class BaseTimeEntity<T extends Serializable>
+        extends BaseIdEntity<T> {
 
     @CreatedDate
     @Builder.Default
--- Spi-Common/src/main/java/com/dgsw/cns/global/mapper/Mapper.java
+++ Spi-Common/src/main/java/com/dgsw/cns/global/mapper/Mapper.java
@@ -3,23 +3,23 @@ package com.dgsw.cns.global.mapper;
 /**
  * Domain 클래스를 Entity 클래스로, Entity 클래스를 Domain 클래스로 변환해주는 인터페이스 <br>
  * 반드시 각각의 타입은 변환이 가능한 형태이어야 합니다.
- * @param <Domain> Domain 모듈에 위치한 Domain 클래스
- * @param <Entity> SPI 모듈에 위치한 Entity 클래스
+ * @param <T> Domain 모듈에 위치한 Domain 클래스
+ * @param <S> SPI 모듈에 위치한 Entity 클래스
  */
-public interface Mapper<Domain, Entity> {
+public interface Mapper<T, S> {
 
     /**
      * Domain 클래스를 인자로 Entity 클래스로 변환합니다.
      * @param domain Domain 인스턴스를 인자로 받습니다.
      * @return Entity 클래스를 반환합니다.
      */
-    Entity domainToEntity(Domain domain);
+    S domainToEntity(T domain);
 
     /**
      * Entity 클래스를 인자로 Domain 클래스로 변환합니다.
      * @param entity Entity 인스턴스를 인자로 받습니다.
      * @return Domain 클래스를 반환합니다.
      */
-    Domain entityToEntity(Entity entity);
+    T entityToEntity(S entity);
 
 }
