52a9f5ab3a90f3faf0147ee189c75046dcdef612
ericlee05 <develretr0@gmail.com>
2023-01-09 14:48:36 +0900

    ✨ :: (#8) create exception handlers and response dto

---
+++ Http-Api/src/main/java/com/dgsw/cns/global/exception/ExceptionResponse.java
@@ -0,0 +1,10 @@
+package com.dgsw.cns.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExceptionResponse {
+    private String message;
+}
