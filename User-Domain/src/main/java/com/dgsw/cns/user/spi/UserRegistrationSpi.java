7ff6b68b0debcde3bc2c983a9076d2eef2fa2676
iqpizza6349 <iqpizza6349@gmail.com>
2023-02-16 00:32:25 +0900

    :ambulance: :: remove heritage of EmailCertificationSpi in UserRegistrationSpi

--- User-Domain/src/main/java/com/dgsw/cns/user/spi/UserRegistrationSpi.java
+++ User-Domain/src/main/java/com/dgsw/cns/user/spi/UserRegistrationSpi.java
@@ -6,10 +6,8 @@ import com.dgsw.cns.user.spi.certification.email.EmailCertificationSpi;
 /**
  * 유저 회원가입을 진행하는 서비스 인터페이스
  * @see com.dgsw.cns.user.spi.certification.PasswordCertificationSpi
- * @see com.dgsw.cns.user.spi.certification.email.EmailCertificationSpi
  */
-public interface UserRegistrationSpi
-        extends PasswordCertificationSpi, EmailCertificationSpi {
+public interface UserRegistrationSpi extends PasswordCertificationSpi {
 
     /**
      * 이메일, 비밀번호, 이메일 인증 코드를 인자로 사용하여 회원가입을 진행합니다.
@@ -21,14 +19,13 @@ public interface UserRegistrationSpi
      * 저장할 대상(이메일)로 전송한 랜덤한 6자리(인증 코드) 역시 인증을 한 후
      * 최종적으로 회원가입이 완료됩니다.
      * <pre>
-     * {@link EmailCertificationSpi#sendCertificationCode(String)}
+     * {@link EmailCertificationSpi#sendCertificationCode(String, Object)}
      * {@link EmailCertificationSpi#matches(String)}
      * </pre>
      *
-     *
-     * @param email
-     * @param password
-     * @param verificationCode
+     * @param email 가입할 이메일
+     * @param password 비밀번호
+     * @param verificationCode 인증 코드
      */
     void register(final String email, final String password, final String verificationCode);
 
