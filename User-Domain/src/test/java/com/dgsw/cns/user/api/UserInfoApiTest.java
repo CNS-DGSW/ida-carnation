c83d377e5bf652d0ff07c02cd93561f209e50abc
iqpizza6349 <iqpizza6349@gmail.com>
2023-02-21 00:41:41 +0900

    :test_tube: :: (#12) add user-info fail test case

--- User-Domain/src/test/java/com/dgsw/cns/user/api/UserInfoApiTest.java
+++ User-Domain/src/test/java/com/dgsw/cns/user/api/UserInfoApiTest.java
@@ -1,43 +1,87 @@
 package com.dgsw.cns.user.api;
 
-import com.dgsw.cns.user.spi.UserInfoSpi;
+import com.dgsw.cns.user.domain.Member;
+import com.dgsw.cns.user.domain.Privacy;
+import com.dgsw.cns.user.domain.enums.Gender;
+import com.dgsw.cns.user.spi.query.QueryPrivacySpi;
+import com.dgsw.cns.user.spi.query.QueryUserSpi;
 import com.dgsw.cns.user.usecase.UserInfoUseCase;
 import com.dgsw.cns.user.vo.MemberInfoVO;
+import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
-/**
- * 순수 JUnit 만을 사용한 테스트이므로 굳이 실패와 동시에 성공 테스트까지 작성할 필요는 없습니다.
- * 물론, @Service 어노테이션이 포함된 클래스를 테스트해야하는 경우, 반드시 실패와 성공 테스트 둘 다 작성해야만 합니다.
- */
+@ExtendWith(MockitoExtension.class)
 class UserInfoApiTest {
 
-    private static final String NAME = "name";
-    private static final String BIRTH = "2023-02-18";
-    private static final String GENDER = "MALE";
-    private static final String TELEPHONE = "010-1111-2222";
+    @Mock
+    private QueryUserSpi queryUserSpi;
 
-    private final UserInfoSpi spi =
-            id -> new MemberInfoVO(NAME, BIRTH, GENDER, TELEPHONE);
-    private final UserInfoApi api = new UserInfoUseCase(spi);
+    @Mock
+    private QueryPrivacySpi queryPrivacySpi;
+
+    @InjectMocks
+    private UserInfoUseCase userInfoUseCase;
+
+    @Test
+    @DisplayName("회원가입을 하지 않은 케이스")
+    void cannotFoundUser() {
+        // given
+        given(queryUserSpi.findUserById(anyLong()))
+                .willThrow(Member.MemberNotFoundException.class);
+
+        // when
+        ThrowableAssert.ThrowingCallable method = () -> userInfoUseCase.getUserInfo(1L);
+
+        // then
+        assertThatThrownBy(method).isInstanceOf(Member.MemberNotFoundException.class);
+    }
 
     @Test
     @DisplayName("회원 정보 조회")
     void findUserById() {
         // given
-        final long memberId = 1L;
+        final Long id = 1L;
+        final String name = "a", telephone = "010-1111-2222";
+        final Member member = Member.builder()
+                .id(id)
+                .name(name)
+                .build();
+        final Privacy privacy = Privacy.builder()
+                .userId(id)
+                .contact(telephone)
+                .gender(Gender.MALE)
+                .birth(LocalDate.now())
+                .build();
+        given(queryUserSpi.findUserById(anyLong()))
+                .willReturn(Optional.of(member));
+        given(queryPrivacySpi.findPrivacyByMemberId(anyLong()))
+                .willReturn(Optional.of(privacy));
 
         // when
-        MemberInfoVO infoVO = api.getUserInfo(memberId);
+        final MemberInfoVO infoVO = userInfoUseCase.getUserInfo(anyLong());
+
+        // then
+        assertThat(infoVO).isNotNull();
+        assertThat(infoVO.getName()).isEqualTo(name);
+        assertThat(infoVO.getBirth()).isNotBlank();
+        assertThat(infoVO.getGender()).isNotBlank();
+        assertThat(infoVO.getTelephone()).isEqualTo(telephone);
 
         // verify
-        assertThat(infoVO).isNotNull();
-        assertThat(infoVO.getName()).isEqualTo(NAME);
-        assertThat(infoVO.getBirth()).isEqualTo(BIRTH);
-        assertThat(infoVO.getGender()).isEqualTo(GENDER);
-        assertThat(infoVO.getTelephone()).isEqualTo(TELEPHONE);
+        verify(queryUserSpi).findUserById(anyLong());
     }
-
 }
