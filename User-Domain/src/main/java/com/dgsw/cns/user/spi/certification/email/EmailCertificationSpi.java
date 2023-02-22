695fc32af82ea6ffd07dc14fa2031572b7e57f39
iqpizza6349 <iqpizza6349@gmail.com>
2023-02-22 18:57:34 +0900

    :alien: :: remove regex default-method

--- User-Domain/src/main/java/com/dgsw/cns/user/spi/certification/email/EmailCertificationSpi.java
+++ User-Domain/src/main/java/com/dgsw/cns/user/spi/certification/email/EmailCertificationSpi.java
@@ -1,17 +1,9 @@
 package com.dgsw.cns.user.spi.certification.email;
 
-import java.util.regex.Pattern;
-
 /**
  * 이메일 인증 코드 발송 및 검증 서비스 인터페이스
  */
 public interface EmailCertificationSpi {
-
-    Pattern EMAIL_REGEX = Pattern.compile("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
-
-    default boolean validationEmail(final String email) {
-        return EMAIL_REGEX.matcher(email).matches();
-    }
 
     /**
      * 받은 이메일 인증코드(6자리)가 전송한 인증코드와 일치 하는 지를 확인 
