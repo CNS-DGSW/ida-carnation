b11acbd94e9b850adde6ca59f1cb93a84abac79d
iqpizza6349 <iqpizza6349@gmail.com>
2023-02-22 20:40:22 +0900

    :alien: :: (#12) add validation annotations in field

--- User-Domain/src/main/java/com/dgsw/cns/user/vo/MemberVeteranVO.java
+++ User-Domain/src/main/java/com/dgsw/cns/user/vo/MemberVeteranVO.java
@@ -1,6 +1,9 @@
 package com.dgsw.cns.user.vo;
 
 import com.dgsw.cns.user.domain.Merit;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -14,11 +17,14 @@ public class MemberVeteranVO {
     /**
      * 발급받은 보훈청 (ex. 대구지방보훈청)
      */
+    @NotBlank
     private final String department;
 
     /**
      * 보훈 번호 (양식: xx-xxxxxx)
      */
+    @Size(min = 9, max = 9, message = "보훈번호는 '-'를 포함하여 9자리입니다.")
+    @Pattern(regexp = "(\\d{2})-(\\d{6})", message = "보훈번호의 양식은 xx-xxxxxx입니다.")
     private final String id;
 
     public Merit toDomain() {
@@ -27,7 +33,6 @@ public class MemberVeteranVO {
                 .department(department)
                 .build();
     }
-
 
     public static MemberVeteranVO fromDomain(Merit merit) {
         return new MemberVeteranVO(merit.getDepartment(), merit.getCode());
